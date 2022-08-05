86262f08dc904852b3eb5af44ccf4d72745628a1
Lili200Cardoso <lili200guedes@gmail.com>
2022-08-05 17:02:23 -0300

    Exercicios curos java basico

---
+++ src/Documentacao/Calculadora.java
@@ -0,0 +1,20 @@
+package Documentacao;
+
+/** <b>Note: </b> Leia atentamente a documentação desta classe
+para desfrutar dos recursos oferecidos pelo autor
+
+@author Lili Cardoso
+@version 1.0
+@since 05/08/2022**/
+
+public class Calculadora {
+
+    /**Este método é utilizado para somar dois números inteiros
+    @param numeroUm este é o primeiro parâmetro do método
+    @param numeroDois este é o segundo parâmetro do método
+    @return int o resultado deste método é a soma dos dois números.**/
+
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
